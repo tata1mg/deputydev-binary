d2a0c6d02b16e57087f156ff3d7b8a98e1e3b980
Pratham verma <pratham.verma@1mg.com>
2025-02-18 15:38:37 +0530

    auth token storing and loading logic added

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,1 +1,12 @@
+from enum import Enum
+
+
 NUMBER_OF_WORKERS = 1
+
+class AuthTokenStorageManagers(Enum):
+    CLI_AUTH_TOKEN_STORAGE_MANAGER = "cli"
+    EXTENSION_AUTH_TOKEN_STORAGE_MANAGER = "extension"
+
+class Status(Enum):
+    SUCCESS = "success"
+    FAILED = "failed"
