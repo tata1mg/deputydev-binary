f039a6b9c998cff21f1181bcdc81d46065959935
sachendra jain <sachendra.jain@1mg.com>
2025-02-24 18:02:38 +0530

    dynamic_port_assigning

--- app/service.py
+++ app/service.py
@@ -1,8 +1,10 @@
 from sanic import Sanic
 from app.routes import binary_blueprints
+import sys
 
 app = Sanic("WebSocketServer")
 app.blueprint(binary_blueprints)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9000, debug=False, legacy=True)
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000  # Default: 8000
+    app.run(host="0.0.0.0", port=port, debug=False, legacy=True)
