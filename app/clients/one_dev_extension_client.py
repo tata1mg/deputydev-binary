f4ab0b9ee48c1faafe03b5ee93c2e9a21dec59f5
sachendra jain <sachendra.jain@1mg.com>
2025-02-28 12:26:43 +0530

    minor changes

---
+++ app/clients/one_dev_extension_client.py
@@ -0,0 +1,16 @@
+from deputydev_core.clients.http.service_clients.one_dev_client import OneDevClient
+from typing import Any, Dict, Optional
+
+
+class OneDevExtensionClient(OneDevClient):
+    async def llm_reranking(self, payload: Dict[str, Any], headers: Dict[str, str]):
+        path = "/end_user/v1/llm_reranking"
+        result = await self.post(url=self._host + path, json=payload, headers=headers)
+        return (await result.json()).get("data")
+
+    async def create_embedding(
+            self, payload: Dict[str, Any], headers: Dict[str, str]
+    ) -> Dict[str, Any]:
+        path = "/end_user/v1/create-embedding"
+        result = await self.post(url=self._host + path, json=payload, headers=headers)
+        return (await result.json()).get("data")
--- app/service.py
+++ app/service.py
@@ -3,17 +3,20 @@ from sanic import Sanic
 from app.routes import binary_blueprints
 import sys
 from app.listeners import listeners
+import multiprocessing
 
 app = Sanic("BinaryServer")
 app.blueprint(binary_blueprints)
 app.config.REQUEST_TIMEOUT = 300
 app.config.RESPONSE_TIMEOUT = 300
 app.config.KEEP_ALIVE_TIMEOUT = 300
+app.config.TOUCHUP = False
 
 for listener in listeners:
     app.register_listener(listener[0], listener[1])
 
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000  # Default: 8000
     app.run(host="0.0.0.0", port=port, debug=False, legacy=True)
