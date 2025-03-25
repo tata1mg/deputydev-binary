053a1b3a7c6018f88215afe4b889f57764598a5b
sachendra jain <sachendra.jain@1mg.com>
2025-03-25 16:57:32 +0530

    progress bar fix

--- app/routes/chunks.py
+++ app/routes/chunks.py
@@ -62,10 +62,10 @@ async def update_vector_store(request, ws):
 
         async def progress_callback(progress):
             """Sends progress updates to the WebSocket."""
-            await ws.send(json.dumps({"status": "In Progress", "progress": progress}))
+            await ws.send(json.dumps({"status": "In Progress", "repo_path": payload.repo_path, "progress": progress}))
 
         await InitializationService.update_chunks(payload, progress_callback)
-        await ws.send(json.dumps({"status": "Completed", "progress": 100}))
+        await ws.send(json.dumps({"status": "Completed", "repo_path": payload.repo_path, "progress": 100}))
 
     except Exception as e:
         # uncomment for local debugging
