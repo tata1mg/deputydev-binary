1aae3874d83ad0ef340fc5a8341891eb5e408b1d
Satyam Kumar <satyam.kumar1@1mg.com>
2025-04-09 11:32:37 +0530

    DD-428 - Ability to read files from X to Y line

--- app/routes/codebase_read.py
+++ app/routes/codebase_read.py
@@ -18,10 +18,13 @@ codebase_read = Blueprint("codebase_read", url_prefix="")
 async def read_file(_request: Request):
     json_body = _request.json
     validated_body = IterativeFileReaderRequestParams(**json_body)
-    file_content = await IterativeFileReader(
+    file_content, eof_reached = await IterativeFileReader(
         file_path=os.path.join(validated_body.repo_path, validated_body.file_path)
     ).read_lines(start_line=validated_body.start_line, end_line=validated_body.end_line)
     response = {
-        "data": file_content.model_dump(mode="json"),
+        "data": {
+            "chunk": file_content.model_dump(mode="json"),
+            "eof_reached": eof_reached,
+        },
     }
     return HTTPResponse(body=json.dumps(response))
--- app/services/codebase_read/iterative_file_reader/iterative_file_reader.py
+++ app/services/codebase_read/iterative_file_reader/iterative_file_reader.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Tuple
 
 import aiofiles
 from deputydev_core.services.chunking.chunk_info import ChunkInfo, ChunkSourceDetails
@@ -20,7 +20,9 @@ class IterativeFileReader:
         self.file_path = file_path
         self.max_lines: int = max_lines or 100
 
-    async def read_lines(self, start_line: int, end_line: int) -> ChunkInfo:
+    async def read_lines(
+        self, start_line: int, end_line: int
+    ) -> Tuple[ChunkInfo, bool]:
         """
         Read a chunk of lines from the file starting from the given offset.
         :param start_line: The line number to start reading from.
@@ -35,31 +37,49 @@ class IterativeFileReader:
 
         async with aiofiles.open(self.file_path, mode="r", encoding="utf-8") as file:
             # Move the file pointer to the start_line
-            for line in range(start_line):
-                await file.readline()
+            for line_iterator in range(start_line - 1):  # 1 indexed
+                line = await file.readline()
+                if not line:
+                    # End of file reached
+                    return (
+                        ChunkInfo(
+                            content="",
+                            embedding=None,
+                            source_details=ChunkSourceDetails(
+                                file_path=self.file_path,
+                                start_line=line_iterator + 1,
+                                end_line=line_iterator + 1,
+                            ),
+                        ),
+                        True,
+                    )
 
             file_content: str = ""
             # Read the next min(max_lines, end_line - start_line) lines or until the end of the file
 
-            actual_line_end: Optional[int] = None
+            actual_line_end: int = start_line
 
-            for line_iterator in range(min(self.max_lines, end_line - start_line)):
+            eof_reached: bool = False
+            for line_iterator in range(
+                min(self.max_lines, end_line - start_line + 1)
+            ):  # 1 indexed
                 line = await file.readline()
                 if not line:
                     # End of file reached
+                    eof_reached = True
                     break
                 actual_line_end = start_line + line_iterator
                 file_content += line
 
-            if not actual_line_end:
-                actual_line_end = start_line + line_iterator
-
-            return ChunkInfo(
-                content=file_content,
-                embedding=None,
-                source_details=ChunkSourceDetails(
-                    file_path=self.file_path,
-                    start_line=start_line,
-                    end_line=actual_line_end,
+            return (
+                ChunkInfo(
+                    content=file_content,
+                    embedding=None,
+                    source_details=ChunkSourceDetails(
+                        file_path=self.file_path,
+                        start_line=start_line,
+                        end_line=actual_line_end,
+                    ),
                 ),
+                eof_reached,
             )
