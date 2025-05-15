2543b1835ba80324db876c0034f68d48d8a5ae5c
deepaks-1mg <deepak.sharma1@1mg.com>
2025-05-15 14:25:15 +0530

    weaviate client changes

--- app/routes/shutdown.py
+++ app/routes/shutdown.py
@@ -1,4 +1,5 @@
 from sanic import Blueprint, Sanic, response
+import asyncio
 
 shutdown = Blueprint("shutdown", url_prefix="")
 
@@ -6,5 +7,20 @@ shutdown = Blueprint("shutdown", url_prefix="")
 @shutdown.get("/shutdown")
 async def shutdown_server(request):
     app = Sanic.get_app()
+
+    # close the weaviate client if it exists
+    if hasattr(app.ctx, "weaviate_client"):
+        weaviate_client = app.ctx.weaviate_client
+        if weaviate_client:
+            await weaviate_client.async_client.close()
+            weaviate_client.sync_client.close()
+
+    # kill the weaviate process if it exists
+    if hasattr(app.ctx, "weaviate_process"):
+        weaviate_process: asyncio.subprocess.Process = app.ctx.weaviate_process
+        if weaviate_process:
+            weaviate_process.terminate()
+            await weaviate_process.wait()
+
     app.stop()
     return response.text("Server shutting down...")
