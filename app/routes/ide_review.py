a097eca6893b830fbce5c05b60655af7de8d5deb
Anand Raj <anand.raj@1mg.com>
2025-06-24 12:05:23 +0530

    minor fixes

--- app/routes/ide_review.py
+++ app/routes/ide_review.py
@@ -8,10 +8,14 @@ from app.services.review.review_service import ReviewService
 review = Blueprint("review", url_prefix="review")
 
 
+def flatten_multidict(multi: dict) -> dict:
+    return {k: v[0] if isinstance(v, list) else v for k, v in multi.items()}
+
+
 @review.route("/new", methods=["GET"])
 async def server_sync(_request: Request):
     try:
-        data = ReviewRequest(**_request.json)
+        data = ReviewRequest(**flatten_multidict(_request.args))
     except ValidationError as e:
         return json({"error": e.errors()}, status=400)
 
@@ -21,14 +25,14 @@ async def server_sync(_request: Request):
 
 @review.route("/branches/get_source_branch", methods=["GET"])
 async def server_sync(_request: Request):
-    repo_path = _request.json["repo_path"]
+    repo_path = _request.args.get("repo_path")
     response = await ReviewService.get_source_branch(repo_path=repo_path)
     return json(response.model_dump())
 
 
 @review.route("/branches/all", methods=["GET"])
 async def server_sync(_request: Request):
-    repo_path = _request.json["repo_path"]
-    keyword = _request.json["keyword"]
+    repo_path = _request.args.get("repo_path")
+    keyword = _request.args.get("keyword")
     response = await ReviewService.search_branches(repo_path=repo_path, keyword=keyword)
     return json(response.model_dump())
