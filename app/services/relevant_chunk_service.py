62b15c943ba6bb1cbaf3496da6c8c0a7926473c2
Vaibhav Meena <vaibhav.meena@1mg.com>
2025-07-16 09:25:25 +0530

    ripgrep added for grep tool and chunking

--- app/services/relevant_chunk_service.py
+++ app/services/relevant_chunk_service.py
@@ -15,15 +15,17 @@ from deputydev_core.utils.constants.enums import ContextValueKeys
 from deputydev_core.utils.weaviate import weaviate_connection
 
 from app.clients.one_dev_client import OneDevClient
+from app.utils.ripgrep_path import get_rg_path
 
 
 class RelevantChunksService:
-    def __init__(self, repo_path):
+    def __init__(self, repo_path: str) -> None:
         self.repo_path = repo_path
 
     async def get_relevant_chunks(self, payload: RelevantChunksParams) -> Dict[str, Any]:
         repo_path = payload.repo_path
         one_dev_client = OneDevClient()
+        ripgrep_path = get_rg_path()
         embedding_manager = ExtensionEmbeddingManager(
             auth_token_key=ContextValueKeys.EXTENSION_AUTH_TOKEN.value,
             one_dev_client=one_dev_client,
@@ -36,8 +38,9 @@ class RelevantChunksService:
                 process_executor=executor,
                 one_dev_client=one_dev_client,
                 weaviate_client=weaviate_client,
+                ripgrep_path=ripgrep_path,
             )
-            relevant_chunks = await CoreRelevantChunksService(repo_path).get_relevant_chunks(
+            relevant_chunks = await CoreRelevantChunksService(repo_path, ripgrep_path).get_relevant_chunks(
                 payload,
                 one_dev_client,
                 embedding_manager,
@@ -49,15 +52,17 @@ class RelevantChunksService:
 
     async def get_focus_chunks(self, payload: FocusChunksParams) -> List[Dict[str, Any]]:
         one_dev_client = OneDevClient()
+        ripgrep_path = get_rg_path()
         with ProcessPoolExecutor(max_workers=ConfigManager.configs["NUMBER_OF_WORKERS"]) as executor:
             initialisation_manager = ExtensionInitialisationManager(
                 repo_path=payload.repo_path,
                 auth_token_key=ContextValueKeys.EXTENSION_AUTH_TOKEN.value,
                 process_executor=executor,
                 one_dev_client=one_dev_client,
+                ripgrep_path=ripgrep_path,
             )
 
-            focus_chunks = await CoreRelevantChunksService(payload.repo_path).get_focus_chunks(
+            focus_chunks = await CoreRelevantChunksService(payload.repo_path, ripgrep_path).get_focus_chunks(
                 payload, initialisation_manager
             )
         return focus_chunks
